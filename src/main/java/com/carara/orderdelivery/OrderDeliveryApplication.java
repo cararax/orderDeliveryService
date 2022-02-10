0c4369a624601162b0caa35847e0ab9bbad562af
Pedro Carara <cararax@gmail.com>
2022-02-10 17:48:14 -0300

    Projeto inicializado

---
+++ src/main/java/com/carara/orderdelivery/OrderDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.carara.orderdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carara/orderdelivery/OrderDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carara.orderdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
